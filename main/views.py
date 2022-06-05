eb379eb842d911e9022e0588073aeda0e621c39d
noam123a <84927552+noam123a@users.noreply.github.com>
2022-06-05 23:20:56 +0300

    signin page

---
+++ main/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, redirect
+from django.urls import path, reverse_lazy
+from django.contrib.auth.views import LoginView
+from . import views
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
+# Create your views here.
+
+
+def mainPage(request):
+    return render(request, 'index.html')
+
+def loginPage(request):
+    return render(request, 'login.html')
+
+
